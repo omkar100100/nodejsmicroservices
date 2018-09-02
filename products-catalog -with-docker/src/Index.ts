a844c57b663bf90b76072b9815795bac65e2b927
chandra sekhar avula <chandrasekhar.avula@fortunapix.com>
2018-09-02 16:31:15 +0530

    folder renamed

--- products-catalog -with-docker/src/Index.ts
+++
@@ -1,6 +0,0 @@
-'use strict';
-//reflect-metadata shim is required, requirement of routing-controllers module.
-import 'reflect-metadata';
-import { Application } from './middleware/config/Application';
-
-export default new Application();
--- products-catalog -with-docker/src/data-layer/data-abstracts/repositories/index.ts
+++
@@ -1,3 +0,0 @@
-export {IProductDocument} from './IProductDocument';
-export {ProductRepo} from './ProductRepository';
-export {ProductSchema} from './ProductSchema';
